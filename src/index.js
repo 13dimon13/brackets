bb249a38a84f0e45dfecbae7d38c51ef67546a23
Дмитрий Колосовский <dmitriy_kolosovskiy@DmiKols-MacBook-Air-13.local>
2019-10-10 17:02:02 +0300

    finish task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,43 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  var arr = str.split('');
+        
+  for(var i = 0; i < bracketsConfig.length; i++){
+
+  var lastIndex = arr.lastIndexOf(bracketsConfig[i][0]);
+
+  var secondIndex = arr.indexOf(bracketsConfig[i][1], lastIndex);
+
+
+  if(lastIndex == 0 && secondIndex == 1 && arr.length == 2){
+
+  arr.splice(lastIndex,2);
+
+  return true;
+
+  }
+
+  while(lastIndex && secondIndex != -1){
+
+  var lastIndex = arr.lastIndexOf(bracketsConfig[i][0]);
+
+  var secondIndex = arr.indexOf(bracketsConfig[i][1], lastIndex);
+
+  arr.splice(lastIndex);
+
+  arr.splice(secondIndex);
+
+  }
+  
+  }
+  if(arr.length > 0){
+
+  return false;
+
+  }
+  else{
+
+  return true; 
+
+  }
 }
